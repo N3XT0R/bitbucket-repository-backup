ef8a88ef5c790d13c81c46748e584556269828d4
Ilya Beliaev <ibeliaev@snud.de>
2023-11-18 10:57:15 +0100

    initial commits

--- src/Config/YamlConfig.php
+++ src/Config/YamlConfig.php
@@ -2,10 +2,12 @@
 
 namespace N3xt0r\BitbucketRepositoryBackup\Config;
 
-use Symfony\Component\Finder\Finder;
+use Symfony\Component\Yaml\Yaml;
 
 class YamlConfig
 {
+
+    protected array $config = [];
 
     protected string $pathToConfig = '';
 
@@ -22,5 +24,28 @@ class YamlConfig
     public function setPathToConfig(string $pathToConfig): void
     {
         $this->pathToConfig = $pathToConfig;
+    }
+
+    public function getConfig(): array
+    {
+        return $this->config;
+    }
+
+    public function setConfig(array $config): void
+    {
+        $this->config = $config;
+    }
+
+    public function loadConfig(): bool
+    {
+        $result = false;
+        $path = $this->getPathToConfig();
+        if (file_exists($path)) {
+            $config = Yaml::parseFile($this->getPathToConfig());
+            $this->setConfig($config);
+            $result = true;
+        }
+
+        return $result;
     }
 }
